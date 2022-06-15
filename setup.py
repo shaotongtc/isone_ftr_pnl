61f6e9da7f4f375b9481f510afac4c9681b8c00f
shaotongtc <59804473+shaotongtc@users.noreply.github.com>
2022-06-15 14:28:05 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='sione_ftr_pnl',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Steve Shao',
+      author_email='stevef.12327@gmail.com'
+     )
